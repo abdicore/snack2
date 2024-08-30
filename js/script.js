1073cc08692167baad884d65a81b702e163265e8
abdicore <abdirhamanmohamed@gmail.com>
2024-08-30 18:53:25 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+/* 2
+ JSnack
+ Il software deve chiedere per 5 volte all’utente di inserire un numero.
+ Il programma stampa la somma di tutti i numeri inseriti.
+ Esegui questo programma in due versioni, con il for e con il while (facoltativo)
+*/
+/* 3
+ JSnack
+ Calcola la somma e la media dei primi 10 numeri.
+*/
+/* 4
+
+ JSnack
+ Chiedi un numero di 4 cifre all’utente
+ e calcola la somma di tutte le cifre che compongono il numero.
+*/
+
+
+
+/* 1
+ JSnack
+ L’utente inserisce due parole in successione, con due prompt.
+ Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+//1-chiedo all utente due parole
+let firstWord = prompt('Dimmi una prima parola')
+let secondWord = prompt('Dimmi una seconda parola')
+
+
+console.log(firstWord,secondWord)
+
+
+
+
+
+//2-stabilre la più corta e piu lunga
+//3- stampa prima la più  cortae poi lunga
+
+
+
+
